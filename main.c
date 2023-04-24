51499dcebe2e17a7ebb4a6f2cd76555d9d92b7a0
junior <abelsouzacosta@gmail.com>
2023-04-24 18:37:08 -0300

    [chore]: matrix
    adding method to print the matrix

--- main.c
+++ main.c
@@ -12,12 +12,39 @@ void fill_tridimentional_matrix(int slices, int rows, int columns, int (*matrix)
   }
 }
 
+void print_tridimensional_matrix(int slices, int rows, int columns, int (*matrix)[rows][columns]) {
+  for (int s = 0; s < slices; s += 1) {
+    printf("[");
+    for (int r = 0; r < rows; r += 1) {
+      printf("[");
+      for (int c = 0; c < columns; c += 1) {
+        if (c == columns - 1) {
+          printf("%d", matrix[s][r][c]);
+        } else {
+          printf("%d, ", matrix[s][r][c]);
+        }
+      }
+      if (r == rows - 1) {
+        printf("]");
+      } else {
+        printf("], ");
+      }
+    }
+    if (s == slices - 1) {
+      printf("]\n");
+    } else {
+      printf("],\n");
+    }
+  }
+}
 
 int main() {
   int matrix[2][2][2];
   int rows, columns;
 
   fill_tridimentional_matrix(2, 2, 2, matrix);
+
+  print_tridimensional_matrix(2, 2, 2, matrix);
 
   return 0;
 }
